9160346174e9253e2ff634f671d39f152ad8fb51
Bryan21developer <bryangarcia21uni@gmail.com>
2024-10-20 22:08:57 -0600

    E1: Funcion que hacepte una lista y devuelva el mayor

---
+++ 1_valormaximoFuncion.py
@@ -0,0 +1,13 @@
+# EJERCICIO:
+# HAZ UNA FUNCION QUE ACEPTE
+# COMO PARAMETROS UNA LISTA Y DEVUELVA EL MAVOR MAXIMO
+milista = [13, 5, 78, 38, 1, 35]
+
+def devuelveMayor(mi_lista):
+    mayor = mi_lista[0]
+    for n in mi_lista:
+        if n > mayor:
+            mayor = n
+    return mayor
+
+print(devuelveMayor(milista))
