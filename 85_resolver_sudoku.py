e326bb42488f0d71e4de41f76cb6d51e14241ea0
Bryan21developer <bryangarcia21uni@gmail.com>
2024-12-08 15:53:17 -0600

    E85: Resolver sudoku backtracking

---
+++ 85_resolver_sudoku.py
@@ -0,0 +1,39 @@
+"""
+Resuelve un sudoku usando backtracking.
+"""
+def es_valido(tablero, fila, col, num):
+    for i in range(9):
+        if tablero[fila][i] == num or tablero[i][col] == num:
+            return False
+        if tablero[3 * (fila // 3) + i // 3][3 * (col // 3) + i % 3] == num:
+            return False
+    return True
+
+def resolver_sudoku(tablero):
+    for fila in range(9):
+        for col in range(9):
+            if tablero[fila][col] == 0:
+                for num in range(1, 10):
+                    if es_valido(tablero, fila, col, num):
+                        tablero[fila][col] = num
+                        if resolver_sudoku(tablero):
+                            return True
+                        tablero[fila][col] = 0
+                return False
+    return True
+
+# Ejemplo de uso
+tablero = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+resolver_sudoku(tablero)
+for fila in tablero:
+    print(fila)
