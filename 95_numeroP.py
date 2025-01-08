c7c3672f3c640c336727cbc184a76a9aed80a02d
Bryan21developer <bryangamer@gmail.com>
2025-01-08 14:01:20 -0600

    E95: numeroP

---
+++ 95_numeroP.py
@@ -0,0 +1,14 @@
+"""
+Determina si un número dado es primo.
+"""
+def es_primo(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+# Ejemplo de uso
+numero = 29
+print(es_primo(numero))  # True
