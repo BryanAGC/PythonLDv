694da75ed1f63917e8d34347b61d8e657b7ccc63
Bryan21developer <bryangamer@gmail.com>
2025-01-08 14:03:14 -0600

    E97:Frecuencia de palabra en frase

---
+++ 97_frecuenciaPalabra.py
@@ -0,0 +1,13 @@
+"""
+Dado un texto, cuenta la frecuencia de cada palabra y devuelve un diccionario con los conteos.
+"""
+from collections import Counter
+
+def contar_palabras(texto):
+    palabras = texto.lower().split()
+    return dict(Counter(palabras))
+
+# Ejemplo de uso
+texto = "hola mundo hola a todos en el mundo"
+print(contar_palabras(texto))  
+# {'hola': 2, 'mundo': 2, 'a': 1, 'todos': 1, 'en': 1, 'el': 1}
