ba730a2b82e9ffaec2acd19d428f7dfc9f64db39
BryanAGC <bryangarcia21uni@gmail.com>
2025-01-18 17:41:57 -0600

    E126:Implementa la compresión de cadenas usando la codificación Run-Length Encoding (RLE).

---
+++ 126_rle.py
@@ -0,0 +1,17 @@
+"""
+Implementa la compresión de cadenas usando la codificación Run-Length Encoding (RLE).
+"""
+def comprimir_rle(cadena):
+    resultado = []
+    i = 0
+    while i < len(cadena):
+        contador = 1
+        while i + 1 < len(cadena) and cadena[i] == cadena[i + 1]:
+            contador += 1
+            i += 1
+        resultado.append(cadena[i] + str(contador))
+        i += 1
+    return ''.join(resultado)
+
+# Ejemplo de uso
+print(comprimir_rle("aaabbcddd"))  # "a3b2c1d3"
