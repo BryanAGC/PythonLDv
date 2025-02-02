6c0c0d74ffc9377144cd4692121ab094222fafec
Bryan21developer <bryangamer@gmail.com>
2025-02-02 14:11:04 -0600

    98:maximoDivisor

---
+++ 98_maximoDivisot.py
@@ -0,0 +1,10 @@
+"""
+Calcula el Máximo Común Divisor (MCD) de dos números utilizando el algoritmo de Euclides.
+"""
+def mcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+# Ejemplo de uso
+print(mcd(48, 18))  # 6
