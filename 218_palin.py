16de74e0082b6e228933981590edef78b32b645d
BryanAGC <bryangarcia21uni@gmail.com>
2025-02-22 21:09:05 -0600

    E218:Escribe una función que determine si una cadena es un palíndromo (se lee igual de izquierda a derecha que de derecha a izquierda).

---
+++ 218_palin.py
@@ -0,0 +1,12 @@
+"""
+Escribe una función que determine si una cadena es un palíndromo (se lee igual de izquierda a derecha que de derecha a izquierda).
+"""
+def es_palindromo(cadena):
+    return cadena == cadena[::-1]
+
+# Ejemplo de uso
+cadena = "madam"
+if es_palindromo(cadena):
+    print(f"{cadena} es un palíndromo.")
+else:
+    print(f"{cadena} no es un palíndromo.")
