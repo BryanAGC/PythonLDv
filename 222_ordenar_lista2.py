551974f3cc1551301864a92e182b00bdea6a17b8
BryanAGC <bryangarcia21uni@gmail.com>
2025-02-24 21:12:18 -0600

    E22:Escribe una función que reciba una lista de números y los ordene de menor a mayor.

---
+++ 222_ordenar_lista2.py
@@ -0,0 +1,10 @@
+"""
+Escribe una función que reciba una lista de números y los ordene de menor a mayor.
+"""
+def ordenar_lista(lista):
+    return sorted(lista)
+
+# Ejemplo de uso
+lista = [7, 2, 9, 1, 5]
+resultado = ordenar_lista(lista)
+print(f"Lista ordenada: {resultado}")
