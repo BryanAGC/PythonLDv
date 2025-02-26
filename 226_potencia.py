87eeea2724fb6040f8d64ccd2fbd6a3b1b13b5af
BryanAGC <bryangarcia21uni@gmail.com>
2025-02-25 21:13:54 -0600

    E226:Escribe una función que calcule la potencia de un número dado elevado a un exponente.

---
+++ 226_potencia.py
@@ -0,0 +1,11 @@
+"""
+Escribe una función que calcule la potencia de un número dado elevado a un exponente.
+"""
+def potencia(base, exponente):
+    return base ** exponente
+
+# Ejemplo de uso
+base = 2
+exponente = 3
+resultado = potencia(base, exponente)
+print(f"{base} elevado a {exponente} es: {resultado}")
