20f218b3d62201619c2c690564212fd5d27eab31
BryanAGC <bryangarcia21uni@gmail.com>
2025-03-09 03:58:53 -0600

    E258

---
+++ 258_es_palindromo.py
@@ -0,0 +1,6 @@
+def es_palindromo(cadena):
+    cadena = cadena.replace(" ", "").lower()
+    return cadena == cadena[::-1]
+
+print(es_palindromo("Anita lava la tina"))
+print(es_palindromo("Hola mundo"))
