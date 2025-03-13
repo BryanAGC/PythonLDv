8d9fe0bdeb96d74cd25256106a2ec0ae25a69533
BryanAGC <bryangarcia21uni@gmail.com>
2025-03-13 04:07:39 -0600

    E269

---
+++ 269_invertir_cadena.py
@@ -0,0 +1,7 @@
+"""
+Escribe una función que reciba una cadena y devuelva la misma cadena, pero invertida.
+"""
+def invertir_cadena(texto):
+    return texto[::-1]
+
+print(invertir_cadena("Hola Mundo"))
