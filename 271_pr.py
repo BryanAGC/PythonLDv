abcbb46f27e4337e28627d0029a2947ed92e65f5
BryanAGC <bryangarcia21uni@gmail.com>
2025-03-13 04:07:52 -0600

    E271

---
+++ 271_pr.py
@@ -0,0 +1,8 @@
+"""
+Escribe una función que reciba un número entero y determine si es par o impar.
+"""
+def es_par(n):
+    return n % 2 == 0
+
+print(es_par(10))  # True
+print(es_par(7))   # False
