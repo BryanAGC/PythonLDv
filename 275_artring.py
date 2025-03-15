b6dfff0a98f86621263e86724badddddd427b013
BryanAGC <bryangarcia21uni@gmail.com>
2025-03-15 04:10:44 -0600

    E275

---
+++ 275_artring.py
@@ -0,0 +1,7 @@
+"""
+Escribe una función que calcule el área de un triángulo dados su base y altura.
+"""
+def area_triangulo(base, altura):
+    return (base * altura) / 2
+
+print(area_triangulo(10, 5))
