fe963d11e726450d0b9de4a9ad3bae74cf8c58ab
BryanAGC <bryangarcia21uni@gmail.com>
2025-03-15 04:11:03 -0600

    E277

---
+++ 277_ordenar_lista.py
@@ -0,0 +1,7 @@
+"""
+Escribe una función que reciba una lista de números y los ordene en orden ascendente.
+"""
+def ordenar_lista(lista):
+    return sorted(lista)
+
+print(ordenar_lista([5, 3, 8, 1, 2]))
