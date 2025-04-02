b5e9e2d0ccdda1506b20fcce373c839001d49c22
BryanAGC <bryangarcia21uni@gmail.com>
2025-04-01 21:08:38 -0600

    E325

---
+++ 325_numpy_producto_escalar.py
@@ -0,0 +1,10 @@
+"""
+Dado dos vectores, calcula su producto escalar.
+"""
+import numpy as np
+
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+producto_escalar = np.dot(a, b)
+
+print("Producto escalar:", producto_escalar)
